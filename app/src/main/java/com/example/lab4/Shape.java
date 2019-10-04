1cb581ba77d28a92b2bb24f4b204ee514d0f9925
Jiahui Wang <34180138+EduardoWang@users.noreply.github.com>
2019-10-03 19:10:10 -0600

    create the project and add Shape.java class

---
+++ app/src/main/java/com/example/lab4/Shape.java
@@ -0,0 +1,16 @@
+package com.example.lab4;
+
+public abstract class Shape {
+    private int x;
+    private int y;
+
+    public Shape() {
+        x = 1;
+        y = 1;
+    }
+
+    public Shape(int x,int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
